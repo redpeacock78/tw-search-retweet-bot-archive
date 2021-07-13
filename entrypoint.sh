76e7305b30f68ad27bfacf5eac7090ee6c6f4bda
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-13 23:38:17 +0900

    fix: Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
 function main() {
+  # MySQLが立ち上がるまで待つ処理
   echo 'waiting for mysqld to be connectable...' &&
     until mysqladmin ping -h mysql_container --silent; do
       sleep 10
     done &&
     echo 'connected!' &&
     {
+      # メイン
       service cron start &&
         crontab cron.conf &&
         yarn start
