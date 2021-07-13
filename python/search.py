b7a4dc5392d9cc76dda8984145eb5614ca9d8658
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-13 23:39:23 +0900

    style: Fix python/search.py

--- python/search.py
+++ python/search.py
@@ -1,10 +1,10 @@
-import twint
 import os
+import twint
 from dotenv import load_dotenv
 load_dotenv()
 
 config = twint.Config()
-config.Search = os.getenv('QUERY')
+config.Search = os.getenv('SEARCH_QUERY')
 config.Limit = os.getenv('SEARCH_LIMIT')
 
 twint.run.Search(config)
