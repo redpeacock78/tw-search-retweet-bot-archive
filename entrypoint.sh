8331773640027f60cf1a5cb7d7b698c93346e684
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-13 20:40:49 +0900

    fix: Fix Docker related

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 function main() {
+  echo 'waiting for mysqld to be connectable...'
   until mysqladmin ping -h mysql_container --silent; do
-    echo 'waiting for mysqld to be connectable...'
-    sleep 2
+    sleep 120
   done
+  echo 'connected!'
   service cron start &&
     crontab cron.conf &&
     yarn start
