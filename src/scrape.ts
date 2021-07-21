0c6f109bc5418105cbf83d7f62bc7b21aca93db6
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-22 08:05:20 +0900

    fix: Fix src/scrape.ts

--- src/scrape.ts
+++ src/scrape.ts
@@ -4,7 +4,7 @@ const scrape: () => Promise<string[]> = async (): Promise<string[]> => {
   try {
     //検索結果をPythonでスクレイピング
     const resp: unknown = await new Promise((resolve, reject): void => {
-      PythonShell.run('python/search.py', null, (err, data): void => {
+      PythonShell.run('libs/search.py', null, (err, data): void => {
         if (err) return reject(err);
         return resolve(data);
       });
