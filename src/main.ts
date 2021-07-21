b57f02e7d00193dd018e1ab2dd0b0b08cb2ee37f
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-21 11:49:36 +0900

    refactor: Fix src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,71 +1,34 @@
 require("dotenv").config();
-import * as retweet from "./retweet";
-import * as scrape from "./scrape";
+import * as data from "./data";
 import * as sleep from "./sleep";
-import mysql = require("mysql2/promise");
+import * as retweet from "./retweet";
 import cron = require("node-cron");
-
-//データベースの設定
-const db_setting = {
-  host: "mysql_container",
-  user: "docker",
-  password: "docker",
-  database: "my_db",
-};
-
-const main: () => Promise<void> = async (): Promise<void> => {
-  const database = await mysql.createConnection(db_setting);
-  try {
-    //データベース側の処理
-    const sql_query =
-      "CREATE TABLE IF NOT EXISTS tweets(id int AUTO_INCREMENT, tweet_id VARCHAR(50), PRIMARY KEY(id))";
-    await database.query(sql_query);
-    const data: [
-      {
-        tweet_id: string;
-      }
-    ] = (await database.query("SELECT tweet_id FROM tweets").then((i) => {
-      return i[0];
-    })) as [{ tweet_id: string }];
-    const data_ids: string[] = data.map((i: { tweet_id: string }): string => {
-      return i.tweet_id;
-    });
-    //データベース側のデータと取得したデータの差分を取得
-    const diff_ids: string[] = (await scrape()).filter(
-      (i: string): boolean => data_ids.indexOf(i) === -1
-    );
-    console.log(`New Tweets: ${diff_ids.length}`);
-    //差分データが存在すればデータの書き込みとretweetを実行
-    if (diff_ids.length !== 0) {
-      //書き込み
-      diff_ids.map(async (i: string): Promise<void> => {
-        const sql_query = "INSERT INTO tweets SET ?";
-        await database.query(sql_query, { tweet_id: i });
-      });
-      //リツイート
-      for (const i of diff_ids) {
-        await retweet(i).then((): void => {
-          console.log(`Retweeted: ${i}`);
-        }).catch((): void => {
-          console.error(`Fatal Retweet: ${i}`);
-        });
-        await sleep(2000);
-      }
-    }
-  } catch {
-    console.log("error");
-  } finally {
-    database.end();
-  }
-};
 
 //一定間隔で実行
 cron.schedule(process.env.NODE_CRON, async (): Promise<void> => {
   try {
+    const error: string[] = [];
     console.log("Running...");
-    await main();
-    console.log("Done!");
-  } catch (e) {
-    console.error(e);
+    await data().then(async (ids: string[]): Promise<void> => {
+      console.log(`New Tweets: ${ids.length}`);
+      for (const i of ids) {
+        await retweet(i)
+          .then((): void => {
+            console.log(`Successfully Retweeted: ${i}`);
+          })
+          .catch((): void => {
+            error.push(`${i}`);
+            console.error(`Failed to Retweet: ${i}`);
+          });
+        await sleep(2000);
+      }
+    });
+    if (!error.length) {
+      console.log("Done!");
+    } else {
+      console.error(`Failed to Retweet:\n  ${error.join("\n  ")}`);
+    }
+  } catch (err: unknown) {
+    console.error(err);
   }
 });
