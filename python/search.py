ba89702c5d86d3ff84cf296a631a05755b8afb8b
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-21 21:04:55 +0900

    feat: Change to handle multiple SEARCH_QUERY, and fix config().Limit not working

--- python/search.py
+++ python/search.py
@@ -1,10 +1,28 @@
 import os
 import twint
 from dotenv import load_dotenv
+from contextlib import redirect_stdout
 load_dotenv()
 
-config = twint.Config()
-config.Search = os.getenv('SEARCH_QUERY')
-config.Limit = os.getenv('SEARCH_LIMIT')
 
-twint.run.Search(config)
+tweets = []
+result = []
+
+
+def scrape():
+    limit = int(os.getenv('SEARCH_LIMIT'))
+    queries = os.getenv('SEARCH_QUERY').split('\n')
+    for query in queries:
+        config = twint.Config()
+        config.Limit = limit
+        config.Search = query
+        config.Store_object = True
+        config.Store_object_tweets_list = tweets
+        with redirect_stdout(open(os.devnull, 'w')):
+            twint.run.Search(config)
+        for tweet in tweets[:limit]:
+            result.append(str(tweet.id))
+    print('\n'.join(result))
+
+
+scrape()
