990dd240a6e1d1d5e04531d871f6551b70735ada
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-22 08:05:36 +0900

    fix: Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 function main() {
   # MySQLが立ち上がるまで待つ処理
   echo 'waiting for mysqld to be connectable...' &&
-    until python3 python/db_check.py 2>/dev/null; do
+    until python3 libs/db_check.py 2>/dev/null; do
       sleep 10
     done &&
     echo 'connected!' &&
