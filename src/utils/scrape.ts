697ba491c8e33f5b27da731b9dfb4e1105d87faf
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-23 23:49:46 +0900

    fix: Fix src/utils/scrape.ts

--- src/utils/scrape.ts
+++ src/utils/scrape.ts
@@ -1,10 +1,18 @@
+require('dotenv').config();
 import { PythonShell } from 'python-shell';
+
+const search_criteria = {
+  env: {
+    SEARCH_QUERY: process.env.SEARCH_QUERY,
+    SEARCH_LIMIT: process.env.SEARCH_LIMIT,
+  },
+};
 
 const scrape: () => Promise<string[]> = async (): Promise<string[]> => {
   try {
     //検索結果をPythonでスクレイピング
     const resp: unknown = await new Promise((resolve, reject): void => {
-      PythonShell.run('libs/search.py', null, (err, data): void => {
+      PythonShell.run('libs/search.py', search_criteria, (err, data): void => {
         if (err) return reject(err);
         return resolve(data);
       });
