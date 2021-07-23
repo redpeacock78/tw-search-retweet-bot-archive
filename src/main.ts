052b8437d02ff859197ad2f5703f625ab678ba63
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-23 12:17:25 +0900

    fix: Fix src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,15 @@
 require('dotenv').config();
-import * as data from './data';
-import * as sleep from './sleep';
-import * as retweet from './retweet';
-import cron = require('node-cron');
+import * as actions from './controller/actions';
+import * as schedule from './controller/schedule';
 
-//一定間隔で実行
-cron.schedule(process.env.NODE_CRON, async (): Promise<void> => {
-  try {
-    const error: string[] = [];
-    console.log('Running...');
-    await data().then(async (ids: string[]): Promise<void> => {
-      console.log(`New Tweets: ${ids.length}`);
-      for (const i of ids) {
-        await retweet(i)
-          .then((): void => {
-            console.log(`Successfully Retweeted: ${i}`);
-          })
-          .catch((err: Error): void => {
-            error.push(err[0].message);
-            console.error(`Failed to Retweet: ${i}`);
-          });
-        await sleep(2000);
-      }
-    });
-    if (!error.length) {
-      console.log('Done!');
-    } else {
-      const logs: {} = error.reduce((prev: {}, current: string): {} => {
-        prev[current] = (prev[current] || 0) + 1;
-        return prev;
-      }, {});
-      console.log(JSON.stringify(logs, null, ' '));
-    }
-  } catch (err: unknown) {
-    console.error(err);
+const cron_time: string = process.env.NODE_CRON;
+const gh_actions: string = process.env.GITHUB_ACTIONS;
+
+//実行
+(async (): Promise<void> => {
+  if (gh_actions === 'true') {
+    await actions();
+  } else {
+    schedule(cron_time);
   }
-});
+})();
