ed0b63e9aeed5ca534313eaf31ae6f0fa79777b2
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-26 03:08:37 +0900

    chore: Fix test/data.spec.ts

--- test/data.spec.ts
+++ test/data.spec.ts
@@ -2,6 +2,11 @@ import * as data from '../src/utils/data';
 
 test('Test src/utils/data.ts', async (): Promise<void> => {
   await data().then((i: string[]): void => {
+    //配列かどうか
     expect(Array.isArray(i)).toBe(true);
+    //要素の型
+    i.map((x: string): void => {
+      expect(typeof x).toBe('string');
+    });
   });
 });
