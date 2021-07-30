125214ae33afa2b8db62bad1ab96fa0a06c1c4d4
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-30 15:28:23 +0900

    feat: Change src/utils/data.ts

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,49 +1,23 @@
+import * as db from './db';
 import * as scrape from './scrape';
-import mysql = require('mysql2/promise');
 
-//データベースの設定
-const db_setting = {
-  host: process.env.DB_HOST ? process.env.DB_HOST : 'mysql_container',
-  port: process.env.DB_HOST ? 33066 : 3306,
-  user: 'docker',
-  password: 'docker',
-  database: 'my_db',
-};
-
-const data: () => Promise<string[]> = async (): Promise<string[]> => {
-  const database: mysql.Connection = await mysql.createConnection(db_setting);
+const data: () => Promise<string[] | Error> = async (): Promise<
+  string[] | Error
+> => {
   try {
-    //データベース側の処理
-    const sql_query =
-      'CREATE TABLE IF NOT EXISTS tweets(id int AUTO_INCREMENT, tweet_id VARCHAR(50), PRIMARY KEY(id))';
-    await database.query(sql_query);
-    const data: [
-      {
-        tweet_id: string;
-      }
-    ] = (await database.query('SELECT tweet_id FROM tweets').then((i) => {
-      return i[0];
-    })) as [{ tweet_id: string }];
-    const data_ids: string[] = data.map((i: { tweet_id: string }): string => {
-      return i.tweet_id;
-    });
     //データベース側のデータと取得したデータの差分を取得
-    const diff_ids: string[] = (await scrape()).filter(
-      (i: string): boolean => data_ids.indexOf(i) === -1
+    const db_data: string[] = (await (await db()).read()) as string[];
+    const scrape_data: string[] = await scrape();
+    const diff_data: string[] = scrape_data.filter(
+      (i: string): boolean => db_data.indexOf(i) === -1
     );
     //差分データが存在すればデータの書き込み
-    if (diff_ids.length !== 0) {
-      diff_ids.map(async (i: string): Promise<void> => {
-        const sql_query = 'INSERT INTO tweets SET ?';
-        await database.query(sql_query, { tweet_id: i });
-      });
+    if (diff_data.length !== 0) {
+      await (await db()).write(diff_data);
     }
-    return diff_ids;
-  } catch (e) {
-    console.error(e);
-    throw new Error();
-  } finally {
-    database.end();
+    return diff_data;
+  } catch (e: unknown) {
+    return e as Error;
   }
 };
 
