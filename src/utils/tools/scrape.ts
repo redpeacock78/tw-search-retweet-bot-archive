7202b1359357ea36a0191c7ec11ad3a1815b2770
redpeacock78 <scarletpeacock78@gmail.com>
2021-08-01 11:50:54 +0900

    refactor: Fix src/utils/tools/scrape.ts

--- src/utils/tools/scrape.ts
+++ src/utils/tools/scrape.ts
@@ -3,29 +3,49 @@ import * as util from 'util';
 import { PythonShell } from 'python-shell';
 import * as child_process from 'child_process';
 
+type py_option = {
+  pythonPath: string;
+  env: {
+    SEARCH_QUERY: string;
+    SEARCH_LIMIT: string;
+  };
+};
+
 const scrape: () => Promise<string[]> = async (): Promise<string[]> => {
   try {
-    //Python-shellに渡す環境変数
-    const exec = util.promisify(child_process.exec);
-    const pypath: string = await exec("type python3|awk '{print $3}'").then(
-      (i: { stdout: string; stderr: string }): string => {
-        return i.stdout.replace('\n', '');
-      }
-    );
-    const search_criteria = {
-      pythonPath: pypath,
-      env: {
-        SEARCH_QUERY: process.env.SEARCH_QUERY,
-        SEARCH_LIMIT: process.env.SEARCH_LIMIT,
-      },
-    };
-    //検索結果をPythonでスクレイピング
-    const py = util.promisify(PythonShell.run);
-    return await py('libs/search.py', search_criteria).then(
-      (i: string[]): string[] => {
-        return i.reverse();
-      }
-    );
+    const query: string = process.env.SEARCH_QUERY;
+    const limit: string = process.env.SEARCH_LIMIT;
+    if (!query && !limit) {
+      throw 'No SEARCH_QUERY and SEARCH_LIMIT is set.';
+    } else if (!query) {
+      throw 'No SEARCH_QUERY is set.';
+    } else if (!limit) {
+      throw 'No SEARCH_LIMIT is set.';
+    } else {
+      //Python-shellに渡す環境変数
+      const exec = util.promisify(child_process.exec);
+      const pypath: string = await exec("type python3|awk '{print $3}'").then(
+        (i: { stdout: string; stderr: string }): string => {
+          return i.stdout.replace('\n', '');
+        }
+      );
+      const search_criteria: py_option = {
+        pythonPath: pypath,
+        env: {
+          SEARCH_QUERY: query,
+          SEARCH_LIMIT: limit,
+        },
+      };
+      //検索結果をPythonでスクレイピング
+      const py: (py_script: string, py_option: py_option) => Promise<string[]> =
+        util.promisify(PythonShell.run);
+      const result: string[] = await py('libs/search.py', search_criteria).then(
+        (i: string[]): string[] => {
+          return i.reverse();
+        }
+      );
+      return result;
+    }
   } catch (err: unknown) {
     throw new Error(err as string);
   }
