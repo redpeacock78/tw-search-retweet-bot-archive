01ee1c522cd66e1b727667365c4a6ead20607be7
redpeacock78 <scarletpeacock78@gmail.com>
2021-08-01 12:07:22 +0900

    chore: Fix test/scrape.spec.ts

--- test/scrape.spec.ts
+++ test/scrape.spec.ts
@@ -1,8 +1,8 @@
-import * as scrape from '../src/utils/scrape';
+import * as scrape from '../src/utils/tools/scrape';
 
 jest.setTimeout(100000);
 
-describe('Test src/utils/scrape.ts', (): void => {
+describe('Test src/utils/tools/scrape.ts', (): void => {
   test('If you have a single search terms', async (): Promise<void> => {
     process.env.SEARCH_LIMIT = '10';
     process.env.SEARCH_QUERY = 'test';
@@ -29,6 +29,27 @@ describe('Test src/utils/scrape.ts', (): void => {
       i.map((x: string): void => {
         expect(typeof x).toBe('string');
       });
+    });
+  });
+  test('If SEARCH_QUERY is not set', async (): Promise<void> => {
+    process.env.SEARCH_QUERY = '';
+    process.env.SEARCH_LIMIT = '10';
+    await scrape().catch((e: Error): void => {
+      expect(typeof e).toBe('object');
+    });
+  });
+  test('If SEARCH_LIMIT is not set', async (): Promise<void> => {
+    process.env.SEARCH_QUERY = 'test';
+    process.env.SEARCH_LIMIT = '';
+    await scrape().catch((e: Error): void => {
+      expect(typeof e).toBe('object');
+    });
+  });
+  test('If SEARCH_QUERY and SEARCH_LIMIT is not set', async (): Promise<void> => {
+    process.env.SEARCH_QUERY = '';
+    process.env.SEARCH_LIMIT = '';
+    await scrape().catch((e: Error): void => {
+      expect(typeof e).toBe('object');
     });
   });
 });
